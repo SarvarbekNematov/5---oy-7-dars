db9bed0e7bc077beb5071bde76ad0861a22a0074
SarvarbekNematov <meoff1011@gmail.com>
2024-06-05 22:25:00 +0500

    dars

---
+++ main.js
@@ -0,0 +1,28 @@
+const box = document.querySelector(".box")
+
+const render = (data) => {
+    box.innerHTML = data.map((item) => `
+        <div>
+            <h1>${item.title}</h1>
+            <button data-delete = ${item.id}>delete</button>
+        </div>
+    `).join("")
+}
+
+const getData = () => {
+    fetch("http://localhost:3600/todos").then((res) => res.json()).then((data) => {
+        render(data)
+    })
+}
+
+getData();
+
+box.addEventListener("click", (e) => {
+    if(e.target.dataset.delete){
+        fetch(`http://localhost:3600/todos/${e.target.dataset.delete}`, {
+            method: 'DELETE'
+        }).then((res) => res.json()).then(() => {
+            getData()
+        })
+    }
+})
